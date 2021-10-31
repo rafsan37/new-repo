9619314aa463151a4841fd37a50619faed42bf49
Rafsan <turjz2827@gmail.com>
2021-10-31 15:43:46 +0600

    added

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwrp9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run (){
+    try{
+        await client.connect();
+        const database = client.db('travelTime');
+        const servicesCollection = database.collection('services');
+        const bookingsCollection = database.collection('bookings');
+
+        console.log('database connented successfully');
+
+        //Get API
+        app.get('/services', async(req, res) =>{
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //GET API
+        app.get('/bookings', async(req,res) =>{
+            const cursor = bookingsCollection.find({});
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+         })
+  
+        //POST API
+         app.post('/bookings', async(req, res) =>{
+            const booking = req.body;
+        console.log('hit the post api', booking);
+  
+         const result = await bookingsCollection.insertOne(booking);
+        console.log(result);
+        res.json(result)
+  });
+
+         // GET Single Service
+         app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+        // Post API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+          const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Travel time Server Is Running');
+})
+app.get('/hello', (req, res) => {
+    res.send('ubdate here');
+})
+
+app.listen(port, () => {
+    console.log('server runnig at port', port);
+})
